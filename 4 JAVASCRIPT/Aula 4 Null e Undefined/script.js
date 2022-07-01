bc28a8e2eb05df100e1d54e81ac128403813c233
Alisson Lindote Novais <lindoten@gmail.com>
2022-07-01 14:01:53 -0300

    Aula 4 - Completa

---
+++ 4 JAVASCRIPT/Aula 4 Null e Undefined/script.js
@@ -0,0 +1,15 @@
+// Null
+// Representa a auxencia intencional de um valor
+
+var teste = null
+
+// Undefined
+// A Variavel nao possui nenhum valor definido!
+
+var teste2
+
+
+//  testando no console  //
+
+console.log(teste)
+console.log(teste2)
