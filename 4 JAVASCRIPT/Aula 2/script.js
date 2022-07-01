6c640e2239e61c719128a4f915c86c187cfa3b0c
Alisson Lindote Novais <lindoten@gmail.com>
2022-07-01 13:25:15 -0300

    Aula 2 - Variaveis Completa

---
+++ 4 JAVASCRIPT/Aula 2/script.js
@@ -0,0 +1,23 @@
+// string
+var texto = `Curso de JavaScript` //Nao precisamos declarar o tipo de variavel string
+
+
+//Number
+var numeroInteiro = -7 //Nao precisamos declarar o tipo de variavel number ou int
+var numeroFracionado = 11210.75 //Nao precisamos declarar o tipo de variavel number ou float
+
+
+
+//Boolean
+var teste = true //true = VERDADEIRO, false = FALSO  //Nao precisamos declarar o tipo de variavel boolean
+
+
+//Para abrir uma janela de alerta no Browser:
+alert(numeroFracionado) //abrir um dialogo
+
+
+//Para escrever no DOM
+document.write(texto)
+
+//Para Escrever no Console do Browser
+console.log(numeroInteiro)
