2ffd822365434debb203870e2b39cdb893be6de6
Alisson Lindote Novais <lindoten@gmail.com>
2022-07-01 12:32:15 -0300

    Aula 1 - Js - Introdução

---
+++ 4 JAVASCRIPT/Aula 1 Js Introdução/meu_script.js
@@ -0,0 +1,12 @@
+alert(`Ola seja bem vindo ao curso!`)
+document.getElementById(`nome`).value = `Oi`
+
+/*
+é assim que se insere comentários em js!
+comentarios, comentarios,...
+comentarios, comentarios,...
+comentarios, comentarios,...
+*/
+
+
+// Ou duas barras para uma unica linha.
