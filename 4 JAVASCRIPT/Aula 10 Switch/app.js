0aff98c19a3f8219e668ae139aeaa465e246da57
Alisson Lindote Novais <lindoten@gmail.com>
2022-07-23 02:31:52 -0300

    Aula 10 - Switch Case

---
+++ 4 JAVASCRIPT/Aula 10 Switch/app.js
@@ -0,0 +1,21 @@
+// COMANDO SWITCH
+var opcao = 4
+
+switch (opcao){
+
+    case 1:
+        document.write('Opcao 1')
+        break
+
+    case 2:
+        document.write('Opcao 2')
+        break
+
+    case 3:
+        document.write('Opcao 3')
+        break
+    
+    default:
+        document.write('Opcao Default')         
+        break
+}
