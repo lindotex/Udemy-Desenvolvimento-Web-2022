8b5562ddcb397f0c71da5ab8d75573c648312691
Alisson Lindote Novais <lindoten@gmail.com>
2022-09-21 13:11:33 -0300

    Aula 05 - Parametros Default

---
+++ 5 ECMA/05_Parametros_Defalt/app.js
@@ -0,0 +1,9 @@
+// ECMA 2015 :: Template Strings
+
+// Função
+
+function contarUmaHistoria(personagem ='Fubá', atividade = 'comer chinelos', nome_dono = 'Jabulani') {
+    document.write(`Era uma vez, um cachorro chamado ${personagem}, ele adorava ${atividade}, seu dono era ${nome_dono}, e eles viveram felizes para sempre.`);
+}
+
+contarUmaHistoria(undefined,'Enterrar Ossos', 'Jubileu');
