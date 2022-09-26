4ecb72407252fc3bed19b7b59a84de20bcf69f34
Alisson Lindote Novais <lindoten@gmail.com>
2022-09-26 18:23:08 -0300

    Atualização da aula 24

--- 5 ECMA/24_Destructuring_assignment/app.js
+++ 5 ECMA/24_Destructuring_assignment/app.js
@@ -1,37 +1,63 @@
 //OOP || Destructuring Assignment
 
-let frutas  = ['Abacaxi', 'Uva', 'Pera', 'Mamao', , , , , ]
+// let frutas  = ['Abacaxi', 'Uva', 'Pera', 'Mamao', , , , , ]
 
-let a = frutas[0]
-let b = frutas[1]
-let c = frutas[2]
+// let a = frutas[0]
+// let b = frutas[1]
+// let c = frutas[2]
 
-console.log(a,b, c)
- ///////////////////////
+// console.log(a,b, c)
+//  ///////////////////////
 
- // Destructuring no Array:
+//  // Destructuring no Array:
 
- let [d, e, , f] = frutas  // [] = Desconstructuring quando a esquerda da atribuição
- // 'd' = indice 0 do array 
- // 'e' = indice 1 do array
- // ' ' = indice 2 do array
- // 'f' = indice 3 do array
+//  let [d, e, , f] = frutas  // [] = Desconstructuring quando a esquerda da atribuição
+//  // 'd' = indice 0 do array 
+//  // 'e' = indice 1 do array
+//  // ' ' = indice 2 do array
+//  // 'f' = indice 3 do array
 
- console.log(d, e, f)
-
-
-// Destructuring no Array com valores padrões:
-
-let [g = 'Pesego', h = 'Banana'] = frutas
-
-console.log(frutas)
-console.log(a,b,c,d,e,f,g, h)
+//  console.log(d, e, f)
 
 
-// ARRAYS
-let coisas = [['Abacaxi', 'Uva', 'Pera', 'Mamao'], ['Jose', 'Maria']]
-let [,[,n2]] = coisas
-let [[ , ,n3],] = coisas
+// // Destructuring no Array com valores padrões:
 
-console.log(n2)
-console.log(n3)
+// let [g = 'Pesego', h = 'Banana'] = frutas
+
+// console.log(frutas)
+// console.log(a,b,c,d,e,f,g, h)
+
+
+// // ARRAYS
+// Token para arrays = []
+// let coisas = [['Abacaxi', 'Uva', 'Pera', 'Mamao'], ['Jose', 'Maria']]
+// let [,[,n2]] = coisas
+// let [[ , ,n3],] = coisas
+
+// console.log(n2)
+// console.log(n3)
+
+//-----------------------------------------------------------------------------
+// Objetos
+// token para objetos = {}
+let produto = {
+    descricao: 'Notebook',
+    preco: 1800,
+    detalhes: {  // Composição  "é um" ou  "tem um" de Objeto em outro Objeto
+        fabricante: 'abc',
+        modelo: 'xyz'
+    }
+}
+
+// let { descricao, preco} = produto
+// console.log(descricao, preco)
+// ou
+
+let { descricao : d, preco: p  } = produto
+console.log(d, p)
+
+// let { descricao, preco, desconto  } = produto
+// console.log(descricao, preco, desconto )
+
+let { descricao, preco, desconto=5, detalhes: {fabricante, modelo} } = produto
+console.log(descricao, preco, desconto, fabricante, modelo )
