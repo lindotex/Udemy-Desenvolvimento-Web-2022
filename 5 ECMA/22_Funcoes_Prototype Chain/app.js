abeab5b7e39e0a20164dcdd55cddf61905066511
Alisson Lindote Novais <lindoten@gmail.com>
2022-09-26 00:18:36 -0300

    Aula 22 Prototype chain

---
+++ 5 ECMA/22_Funcoes_Prototype Chain/app.js
@@ -0,0 +1,11 @@
+//Object Prototype || Prototype Chain
+
+Object.prototype.attr50 = "z"
+
+
+// Literal
+let animal = { attr1: 'a'}
+    let vertebrado ={__proto__: animal, attr2: 'b',}
+        let ave ={__proto__: vertebrado, attr3: 'c', attr2: 'x'}
+
+console.log(ave.attr2, ave.attr1, ave.attr3, ave.attr50)
