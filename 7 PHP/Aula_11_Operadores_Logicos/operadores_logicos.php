64a14bc1b16502c28d55506d176da4adf4a58b46
Alisson Lindote Novais <lindoten@gmail.com>
2022-09-29 15:45:35 -0300

    Aula 11 - Operadores Logicos

---
+++ 7 PHP/Aula_11_Operadores_Logicos/operadores_logicos.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variaveis Prática</title>
+</head>
+    <?php
+
+        #### Operadores Logicos ####
+        // Operador E: AND ou &&
+        // Operador OU: OR ou ||
+        // Operador xor: XOR
+        // Operador Negacao: !=
+
+
+
+    ?>
+
+<body>
+
+
+</body>
+</html>
+
