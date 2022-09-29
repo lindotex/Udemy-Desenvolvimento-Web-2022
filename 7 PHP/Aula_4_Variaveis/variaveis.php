48efd0c634a9f07368ce22f02c0e6259a22aebc3
Alisson Lindote Novais <lindoten@gmail.com>
2022-09-29 00:15:29 -0300

    aula 4 - variaveis

---
+++ 7 PHP/Aula_4_Variaveis/variaveis.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variaveis</title>
+</head>
+    <?php
+
+        echo '<h1>Variaveis em PHP</h1><br/>';
+        echo '<p>Sintaxe de declaração de variaveis em php:</p>'
+
+    ?>
+    <table>
+        <tr>
+            <th>Tipos de variaveis</th>
+            <th>Significado</th>
+        </tr>
+        <tr>
+            <td>String</td>
+            <td>Texto</td>
+        </tr>
+        <tr>
+            <td>int</td>
+            <td>Números inteiros, sem decimal ou fração</td>
+        </tr>
+        <tr>
+            <td>float</td>
+            <td>Números com decimais ou funções</td>
+        </tr>
+        <tr>
+            <td>Boolean</td>
+            <td>Assumem dois valores(True ou False, 1 ou 0)</td>
+        </tr>
+        <tr>
+            <td>Array</td>
+            <td>Variavel que contém blocos de dados ordenados de outros tipos.</td>
+        </tr>
+    </table>
+
+    <?php
+        echo '<p>Regras para declaração de variaveis:</p>'
+        ?>
+    <ol><li>Deve Obrigatoriamente iniciar com $.</li><li>Não podem conter espaços vazios e caracteres especiais apenas _ </li></ol>'
+
+    <?php
+        echo 'A linguaqem php é case sensitive!'
+        ?>
+    <p>$Nome != $NOME != $nome != $NoMe</p>'
+
+    <?php
+        echo 'Exemplos validos de nomes de funções'
+        ?>
+    <p>$nome, $fone1, $endereco_2, ...</p>'
+
+<body>
+
+
+</body>
+</html>
+
