9bbd48cb4326232de8aa045abc486703c7c07f1c
Alisson Lindote Novais <lindoten@gmail.com>
2022-09-29 00:25:34 -0300

    Aula 5  - Praticando com variaveis

---
+++ 7 PHP/Aula_5_Variaveis_Pratica/variaveis_praticas.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variaveis Prática</title>
+</head>
+    <?php
+
+        //string
+        $nome = 'Alisson lindote Novais';
+
+        //int
+        $idade = '29 anos';
+
+        //float
+        $peso = '82,5 kg';
+
+        //boolean
+        $fumante = true;
+
+    ?>
+
+    <h1>Ficha cadastral</h1>
+    <br>
+    <p>Nome: <?= $nome?></p> <!-- tag de impressão -->
+    <p>Idade: <?= $idade?></p>
+    <p>Peso: <?= $peso?></p>
+    <p>Fumante: <?= $fumante?></p>
+
+<body>
+
+
+</body>
+</html>
+
