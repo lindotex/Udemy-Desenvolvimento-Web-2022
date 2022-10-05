26aacb05dd4876507f70e41459dfaf2a5b7ea56f
Alisson Lindote Novais <lindoten@gmail.com>
2022-10-05 20:37:53 -0300

    Aula 18 - Incremento e Decremento Concluida

---
+++ 7 PHP/Aula_18_Incremento_decremento/operador_incremento_decremento.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>operador_incremento_decremento</title>
+</head>
+    <h2>operador_incremento_decremento</h2>
+
+    <?php
+
+        // Operador de Incremento ++
+        // Operador de Decremento
+        // Pode ser usado antes ou depois da variavel.
+
+        //Pre incremento.
+        $a = 1;
+        echo "<br/>$a";
+        ++$a;
+        echo "<br/>$a";
+        ++$a;
+        echo "<br/>$a";
+        ++$a;
+        echo "<br/>$a";
+        ++$a;
+        echo "<br/>$a";
+        ++$a;
+        echo "<br/>$a<br/>";
+
+
+        //Pros incremento.
+        $a = 1;
+        echo "<br/>$a";
+        $a++;
+        echo "<br/>$a";
+        $a++;
+        echo "<br/>$a";
+        $a++;
+        echo "<br/>$a";
+        $a++;
+        echo "<br/>$a";
+        $a++;
+        echo "<br/>$a";
+    ?>
+
+
+<body>
+
+
+</body>
+</html>
+
