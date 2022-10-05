76383265af5915dcf581161ba93da6c79f14e9c7
Alisson Lindote Novais <lindoten@gmail.com>
2022-10-05 20:32:20 -0300

    Aula 17 completa

---
+++ 7 PHP/Aula_17_Atribuicao_de_valor/atribuicao_de_valor.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atribuicao de valores</title>
+</head>
+    <h2>Atribuicao de valores</h2>
+
+    <?php
+
+        //Atribuicao '="
+        //Outros: +, -, *, /.
+
+
+        $x = 10;
+
+        $x += +5; // += atribuicao com soma.
+
+        echo $x;  // Resultado esperado = 15.
+
+
+        $y = 8;
+
+        $x += $y; //Esperado 15 + 8 = 23
+        echo "<br/>$x";
+
+
+    ?>
+
+
+<body>
+
+
+</body>
+</html>
+
