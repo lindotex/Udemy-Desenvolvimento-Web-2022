ce233a7bd604c064f47c4f61a1797f8802008a08
lindotex <lindoten@gmail.com>
2022-10-31 09:53:15 -0300

    APP Lista Tarefas - Tarefas.service

---
+++ 0 Projetos/Projeto_15_PDO_APP_Lista_de_tarefas/Back-end/tarefa.service.php
@@ -0,0 +1,17 @@
+<?php
+    //CRUD
+    class TarefaService {
+        public function inserir(){ // CREATE
+
+        }
+        public function recuperar(){ // READ
+
+        }
+        public function atualizar(){ // UPDATE
+
+        }
+        public function remover(){ // DELETE
+
+        }
+    }
+?>
