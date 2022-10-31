66a41a5282a57f9279765b6a1d9b3a3993dbfeb9
lindotex <lindoten@gmail.com>
2022-10-31 11:08:28 -0300

    App Lista Tarefas - Inserindo registros

--- 0 Projetos/Projeto_15_PDO_APP_Lista_de_tarefas/Back-end/tarefa.service.php
+++ 0 Projetos/Projeto_15_PDO_APP_Lista_de_tarefas/Back-end/tarefa.service.php
@@ -1,8 +1,21 @@
 <?php
     //CRUD
     class TarefaService {
-        public function inserir(){ // CREATE
 
+        public $conexao;
+        public $tarefa;
+
+        public function __construct(Conexao $conexao, Tarefa $tarefa)
+        {
+            $this->conexao = $conexao->conectar();
+            $this->tarefa = $tarefa;
+        }
+
+        public function inserir(){ // CREATE
+            $query = 'INSERT INTO tb_tarefas(tarefa) VALUES(:tarefa)';
+            $stmt = $this->conexao->prepare($query);
+            $stmt->bindValue(':tarefa', $this->tarefa->__get('tarefa'));
+            $stmt->execute();
         }
         public function recuperar(){ // READ
 
--- 0 Projetos/Projeto_15_PDO_APP_Lista_de_tarefas/Back-end/tarefa_controller.php
+++ 0 Projetos/Projeto_15_PDO_APP_Lista_de_tarefas/Back-end/tarefa_controller.php
@@ -1,5 +1,14 @@
 <?php
-    echo '<pre>';
-    print_r($_POST);
-    echo '</pre>';
+    require '../Back-end/tarefa.model.php';
+    require '../Back-end//tarefa.service.php';
+    require '../Back-end/conexao.php';
+
+    $tarefa = new Tarefa();
+    $tarefa->__set('tarefa', $_POST['tarefa']);
+
+    $conexao = new Conexao();
+
+    $tarefaService = new TarefaService($conexao, $tarefa);
+    $tarefaService->inserir();
+
 ?>
