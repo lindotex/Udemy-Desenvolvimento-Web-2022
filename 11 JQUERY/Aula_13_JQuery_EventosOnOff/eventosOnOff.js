85515abbae4a31e39846aee92270b8e9481dc969
lindotex <lindoten@gmail.com>
2022-12-02 19:23:12 -0300

    Aula 13 - Eventos On e Off

---
+++ 11 JQUERY/Aula_13_JQuery_EventosOnOff/eventosOnOff.js
@@ -0,0 +1,8 @@
+// jQuery => On and Off Event Commands
+
+$(document).on()
+$(document).off()
+
+$(document).live()      // Descontinuado na versão 3 do jQuery
+$(document).delegate()  // Descontinuado na versão 3 do jQuery
+$(document).bind()      // Descontinuado na versão 3 do jQuery
