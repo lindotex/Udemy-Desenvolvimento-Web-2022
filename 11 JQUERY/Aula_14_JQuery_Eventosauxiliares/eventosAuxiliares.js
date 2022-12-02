9098c3f0c4aa085e7894a96c0bfd36a3e8a5b3a5
lindotex <lindoten@gmail.com>
2022-12-02 20:34:08 -0300

    Aula 14 - Trigger, Hover e ToggleClass

---
+++ 11 JQUERY/Aula_14_JQuery_Eventosauxiliares/eventosAuxiliares.js
@@ -0,0 +1,6 @@
+// jQuery => Auxiliaries Event Commands
+
+$(document).trigger()           //  Aciona evento de modo programático
+$(document).hover()             //  Função auxiliar para captura dos eventos mouseenter/mouseout/mouseover
+$(document).toggleClass()       //  Função auxiliar para inclusão/remoção de classe
+
