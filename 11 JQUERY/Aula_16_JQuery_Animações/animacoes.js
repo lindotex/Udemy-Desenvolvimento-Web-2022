b8807e14f9ad9b55026e0eabe0f0625699d331a9
lindotex <lindoten@gmail.com>
2022-12-04 03:05:22 -0300

    aula 16

---
+++ 11 JQUERY/Aula_16_JQuery_Animações/animacoes.js
@@ -0,0 +1,10 @@
+// jQuery => Animation Effects Commands
+
+
+// Efeitos de animação:
+
+// .animate() -> Pode operar em qualquer propriedade CSS que tenha atributos numéricos.
+// propriedades visuais -> Recebe um Objeto literal contendo
+// Opções de transição -> Recebe um objeto literal contendo
+
+$(document).animate({'propriedades visuais'}, {'opções de transição'})
