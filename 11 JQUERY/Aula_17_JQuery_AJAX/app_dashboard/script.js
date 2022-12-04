266b807ff96bbde56180e4a555322926042c60b9
Alisson Lindote Novais <lindoten@gmail.com>
2022-12-04 03:41:27 -0300

    Aula 17 - load(), get(), post()

--- 11 JQUERY/Aula_17_JQuery_AJAX/app_dashboard/script.js
+++ 11 JQUERY/Aula_17_JQuery_AJAX/app_dashboard/script.js
@@ -1,3 +1,35 @@
 $(document).ready(() => {
-	
+	$('#documentacao').on('click', ()=>{
+        console.log('Link Documentação Clicado!');
+
+        // carregando a pagina utilizando o método 'load()'
+        // $('#pagina').load('documentacao.html');
+
+        // carregando a pagina utilizando o método 'get()'
+        // $.get('documentacao.html', data => {
+        //     $('#pagina').html(data)
+        // });
+
+        // carregando a pagina utilizando o método 'post()'
+        $.post('documentacao.html', data => {
+            $('#pagina').html(data)
+        });
+
+    })
+	$('#suporte').on('click', ()=>{
+        console.log('Link Suporte Clicado!');
+
+        // carregando a pagina utilizando o método 'load()'
+        // $('#pagina').load('suporte.html');
+
+        // carregando a pagina utilizando o método 'get()'
+        // $.get('suporte.html', data => {
+        //     $('#pagina').html(data)
+        // });
+
+        // carregando a pagina utilizando o método 'post()'
+        $.post('suporte.html', data => {
+            $('#pagina').html(data)
+        });
+    })
 })
