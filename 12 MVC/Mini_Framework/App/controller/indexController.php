31498c428d9bf65a56e8e3abe95895f67c476e56
lindotex <lindoten@gmail.com>
2022-12-05 08:53:19 -0300

    Action Controller

--- 12 MVC/Mini_Framework/App/controller/indexController.php
+++ 12 MVC/Mini_Framework/App/controller/indexController.php
@@ -2,13 +2,9 @@
 
     namespace App\Controllers;
 
-    class indexController {
+    use MF\Controller\Action;
 
-        private $view;
-
-        public function __construct(){
-            $this->$view = new \stdClass();
-        }
+    class indexController extends Action {
 
         public function index() {
             $this->$view->$dados = array('Sofá', 'Cadeira', 'Cama');  // para Simular um dado vindo do Model
@@ -18,13 +14,6 @@
         public function sobreNos(){
             $this->$view->$dados = array('Notebook', 'Smartphone');   // para Simular um dado vindo do Model
             $this->render('sobreNos');
-        }
-
-        public function render($view) {
-            $classAtual = get_class($this);
-            $classAtual = str_replace('App\\Controllers\\', '', $classAtual);
-            $classAtual = strtolower(str_replace('Controller', '', $classAtual));
-            require_once "../View/".$classAtual."/".$view.".phtml";
         }
     }
 
