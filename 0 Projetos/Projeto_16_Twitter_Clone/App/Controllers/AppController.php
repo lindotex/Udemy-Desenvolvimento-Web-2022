fbe4c26540d765cbaefde22c4145b338a2c212a1
Alisson Lindote Novais <lindoten@gmail.com>
2022-12-09 18:16:37 -0300

    Class Tweet

--- 0 Projetos/Projeto_16_Twitter_Clone/App/Controllers/AppController.php
+++ 0 Projetos/Projeto_16_Twitter_Clone/App/Controllers/AppController.php
@@ -9,14 +9,33 @@
     class AppController extends Action {
 
         public function timeline(){
-            session_start();
+            $this->validaAutenticacao();
+            // Recuperação dos tweets
 
-            if($_SESSION['id'] != '' && $_SESSION['nome'] != ''){
-                $this->render('timeline');
-            } else {
+            $tweet = Container::getModel('Tweet');
+            $tweet->__set('id_usuario', $_SESSION['id']);
+            $tweets = $tweet->getAll();
+            $this->view->tweets = $tweets;
+
+            $this->render('timeline');
+        }
+
+        public function tweet(){
+            $this->validaAutenticacao();
+
+            $tweet = Container::getModel('Tweet');
+
+            $tweet->__set('tweet', $_POST['tweet']);
+            $tweet->__set('id_usuario', $_SESSION['id']);
+            $tweet->salvar();
+            header('Location: /timeline');
+        }
+
+        public function validaAutenticacao(){
+            session_start();
+            if(!isset($_SESSION['id']) || $_SESSION['id'] == '' || !isset($_SESSION['nome']) || $_SESSION['nome'] == ''){
                 header('Location: /?login=erro');
             }
-
         }
     }
 
