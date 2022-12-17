4647b91e29d409a02460fdb6a1d6ca4a11e0c78d
lindotex <lindoten@gmail.com>
2022-12-17 19:15:53 -0300

    data biding class

--- 14-IONIC/ionicApp/src/app/home/home.page.ts
+++ 14-IONIC/ionicApp/src/app/home/home.page.ts
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public titulo: String = "Meu primeiro App";
+  public imagemRamdomica: String = "https://images.unsplash.com/photo-1661956603025-8310b2e3036d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80";
+  public imagemLocal: String = "../../assets/icone-celular.png";
 
+  public atualiza(): void {
+    this.titulo = "Texto alterado";
+  }
+  public acao(): void {
+    this.titulo = "Botao clicado";
+  }
 }
